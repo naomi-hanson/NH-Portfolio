86f42cfa83db16cab5aba2e60922f3998f390f51
Lauren Leutheuser <laurenleutheuser@laurens-mbp.attlocal.net>
2017-03-26 01:08:46 -0700

    rounded corners

---
+++ js/activeLink-scroll.js
@@ -0,0 +1,19 @@
+$(window).scroll(function() {
+    var windscroll = $(window).scrollTop();
+    if (windscroll >= 100) {
+        $('nav').addClass('fixed');
+        $('.wrapper section').each(function(i) {
+            if ($(this).position().top <= windscroll - 100) {
+                $('nav a.active').removeClass('active');
+                $('nav a').eq(i).addClass('active');
+            }
+        });
+
+    } else {
+
+        $('nav').removeClass('fixed');
+        $('nav a.active').removeClass('active');
+        $('nav a:first').addClass('active');
+    }
+
+}).scroll();​
